68dd7eed5c634cff1d0a8859a21510840fafffab
daiki Iijima <daiki.applecreate@gmail.com>
2023-04-12 15:16:37 +0900

    プロジェクト ファイルを追加します。

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,3 @@
+﻿class FizzBuzz {
+
+}
---
+++ TDD_FizzBuzz/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace TDD_FizzBuzz {
+    public class Tests {
+        [SetUp]
+        public void Setup() {
+        }
+
+        [Test]
+        public void Test1() {
+            Assert.Pass();
+        }
+    }
+}

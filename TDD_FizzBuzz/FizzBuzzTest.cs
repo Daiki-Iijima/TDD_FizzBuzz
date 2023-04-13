1531b7379682e847e17f3b02c7cc5510dbcd6a25
daiki Iijima <daiki.applecreate@gmail.com>
2023-04-13 14:15:25 +0900

    [fix] テストを構造化してドキュメントとして扱えるように

--- TDD_FizzBuzz/FizzBuzzTest.cs
+++ TDD_FizzBuzz/FizzBuzzTest.cs
@@ -1,37 +1,58 @@
 using NUnit.Framework;
 
-namespace TDD_FizzBuzz {
-    public class Tests {
-        private FizzBuzz fizzBuzz;
+namespace FizzBuzz”—ñ‚Æ•ÏŠ·‹K‘¥‚ğˆµ‚¤FizzBuzzƒNƒ‰ƒX {
+    public class  FizzBuzz_Test{
+        public class _3‚Ì”{”‚Ì‚Í”‚Ì‘ã‚í‚è‚ÉFizz‚É•ÏŠ·‚·‚é {
+            private FizzBuzz fizzBuzz;
 
-        [SetUp]
-        public void Setup() {
-            //  €”õ
-            fizzBuzz = new FizzBuzz();
+            [SetUp]
+            public void Setup() {
+                //  €”õ
+                fizzBuzz = new FizzBuzz();
+            }
+
+            [Test]
+            public void _3‚ğ“n‚·‚Æ•¶š—ñFizz‚ğ•Ô‚·() {
+                //  Às & ŒŸØ
+                Assert.AreEqual("Fizz", fizzBuzz.Convert(3));
+            }
         }
 
-        [Test]
-        public void _1‚ğ“n‚·‚Æ•¶š—ñ1‚ğ•Ô‚·() {
-            //  Às & ŒŸØ
-            Assert.AreEqual("1", fizzBuzz.Convert(1));
+        public class _5‚Ì”{”‚Ì‚Í”‚Ì‘ã‚í‚è‚ÉBuzz‚É•ÏŠ·‚·‚é {
+            private FizzBuzz fizzBuzz;
+
+            [SetUp]
+            public void Setup() {
+                //  €”õ
+                fizzBuzz = new FizzBuzz();
+            }
+
+            [Test]
+            public void _5‚ğ“n‚·‚Æ•¶š—ñBuzz‚ğ•Ô‚·() {
+                //  Às & ŒŸØ
+                Assert.AreEqual("Buzz", fizzBuzz.Convert(5));
+            }
         }
 
-        [Test]
-        public void _2‚ğ“n‚·‚Æ•¶š—ñ2‚ğ•Ô‚·() {
-            //  Às & ŒŸØ
-            Assert.AreEqual("2", fizzBuzz.Convert(2));
-        }
+        public class ‚»‚Ì‘¼‚Ì”‚Ì‚Æ‚«‚Í‚»‚Ì‚Ü‚Ü•¶š—ñ‚É•ÏŠ·‚·‚é {
+            private FizzBuzz fizzBuzz;
 
-        [Test]
-        public void _3‚ğ“n‚·‚Æ•¶š—ñFizz‚ğ•Ô‚·() {
-            //  Às & ŒŸØ
-            Assert.AreEqual("Fizz", fizzBuzz.Convert(3));
-        }
+            [SetUp]
+            public void Setup() {
+                //  €”õ
+                fizzBuzz = new FizzBuzz();
+            }
+            [Test]
+            public void _1‚ğ“n‚·‚Æ•¶š—ñ1‚ğ•Ô‚·() {
+                //  Às & ŒŸØ
+                Assert.AreEqual("1", fizzBuzz.Convert(1));
+            }
 
-        [Test]
-        public void _5‚ğ“n‚·‚Æ•¶š—ñBuzz‚ğ•Ô‚·() {
-            //  Às & ŒŸØ
-            Assert.AreEqual("Buzz", fizzBuzz.Convert(5));
+            [Test]
+            public void _2‚ğ“n‚·‚Æ•¶š—ñ2‚ğ•Ô‚·() {
+                //  Às & ŒŸØ
+                Assert.AreEqual("2", fizzBuzz.Convert(2));
+            }
         }
     }
 }

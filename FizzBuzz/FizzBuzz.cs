c76fd0c38c410795d4c1e43cc63f22d131439c54
daiki Iijima <daiki.applecreate@gmail.com>
2023-04-13 13:20:25 +0900

    [add] fizz,buzzを返す処理を作成

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -1,3 +1,14 @@
-﻿class FizzBuzz {
+﻿public class FizzBuzz {
+    public string Convert(int i) {
+        //  3の倍数の場合
+        if (i % 3 == 0) {
+            return "Fizz";
+        }
 
+        //  5の倍数の場合
+        if (i % 5 == 0) {
+            return "Buzz";
+        }
+        return string.Concat(i);
+    }
 }
--- TDD_FizzBuzz/FizzBuzzTest.cs
+++ TDD_FizzBuzz/FizzBuzzTest.cs
@@ -2,13 +2,36 @@ using NUnit.Framework;
 
 namespace TDD_FizzBuzz {
     public class Tests {
+        private FizzBuzz fizzBuzz;
+
         [SetUp]
         public void Setup() {
+            //  €”õ
+            fizzBuzz = new FizzBuzz();
         }
 
         [Test]
-        public void Test1() {
-            Assert.Pass();
+        public void _1‚ğ“n‚·‚Æ•¶š—ñ1‚ğ•Ô‚·() {
+            //  Às & ŒŸØ
+            Assert.AreEqual("1", fizzBuzz.Convert(1));
+        }
+
+        [Test]
+        public void _2‚ğ“n‚·‚Æ•¶š—ñ2‚ğ•Ô‚·() {
+            //  Às & ŒŸØ
+            Assert.AreEqual("2", fizzBuzz.Convert(2));
+        }
+
+        [Test]
+        public void _3‚ğ“n‚·‚Æ•¶š—ñFizz‚ğ•Ô‚·() {
+            //  Às & ŒŸØ
+            Assert.AreEqual("Fizz", fizzBuzz.Convert(3));
+        }
+
+        [Test]
+        public void _5‚ğ“n‚·‚Æ•¶š—ñBuzz‚ğ•Ô‚·() {
+            //  Às & ŒŸØ
+            Assert.AreEqual("Buzz", fizzBuzz.Convert(5));
         }
     }
 }
